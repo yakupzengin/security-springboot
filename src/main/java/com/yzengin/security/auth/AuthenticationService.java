e5c1f9fba2299c9ad09bc7621e0cfb586dad2d70
Yakup Zengin <clafedersin@gmail.com>
2024-04-24 21:17:56 +0300

    Added Comment Lines: AuthenticationService.java

--- src/main/java/com/yzengin/security/auth/AuthenticationService.java
+++ src/main/java/com/yzengin/security/auth/AuthenticationService.java
@@ -1,3 +1,9 @@
+/*
+     This class handles user authentication-related operations such as user registration and authentication.
+     It interacts with the user repository for user data manipulation, password encoder for password hashing,
+     JWT service for token generation and validation, and authentication manager for user authentication.
+
+ */
 package com.yzengin.security.auth;
 
 import com.yzengin.security.config.JwtService;
@@ -17,7 +23,16 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+
+    /*
+        Registers a new user with the provided registration request.
+        Creates a new user entity, saves it to the repository, generates an authentication token
+        and returns it as part of the response.
+
+     */
     public AuthenticationResponse register(RegisterRequest request) {
+        // Create a new user entity with the provided details and encode the password.
+
         var user = User.builder()
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
@@ -26,13 +41,25 @@ public class AuthenticationService {
                 .role(Role.USER)
                 .build();
         repository.save(user);
+
+        // Generate an authentication token for the registered user.
         var jwtToken =jwtService.generateToken(user);
+
+        // Build and return the authentication response containing the token.
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
     }
 
+    /*
+        Authenticates a user with the provided authentication request.
+        Attempts to authenticate the user with the authentication manager, retrieves the user details,
+        generates an authentication token, and returns it as part of the response.
+
+     */
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+        // Authenticate the user with the provided credentials using the authentication manager.
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
